71955c35985f600ab4059f22ed5863e6c9f1a702
shishb <shishb@pku.edu.cn>
2017-06-21 10:25:02 +0800

    add FileUtil class

--- src/main/java/cn/com/lemon/util/file/FileUtil.java
+++ src/main/java/cn/com/lemon/util/file/FileUtil.java
@@ -35,6 +35,7 @@ public class FileUtil {
 
 	/* ==============folder handler=============== */
 	public static boolean createFolder(String folder) {
+		LOG.debug("=====create folder=====");
 		boolean success = true;
 		File fileDirectory = new File(folder);
 		if (!fileDirectory.exists()) {
@@ -44,6 +45,7 @@ public class FileUtil {
 	}
 
 	public static boolean deleteFolder(String folder) {
+		LOG.debug("=====delete folder=====");
 		boolean success = true;
 		File fileDirectory = new File(folder);
 		if (fileDirectory.exists()) {
@@ -54,59 +56,61 @@ public class FileUtil {
 
 	/* ==============copy file=============== */
 	public static File createFile(String filePath, String fileName, boolean replace) {
+		LOG.debug("=====create file=====");
 		return createFile(filePath, fileName, null, replace);
 	}
 
-	public static File createFile(String filePath, String fileName, String fileContent, boolean replace) {
-		if ((filePath == null) || (filePath.equals("")) || (fileName == null) || (fileName.equals(""))) {
+	public static File createFile(String path, String name, String content, boolean replace) {
+		LOG.debug("=====create file=====");
+		if ((path == null) || (path.equals("")) || (name == null) || (name.equals(""))) {
 			return null;
 		}
-		File desFile = null;
 		try {
-			desFile = new File(filePath + (filePath.endsWith("/") ? "" : "/") + fileName);
+			File desFile = new File(path + (path.endsWith("/") ? "" : "/") + name);
 			if (desFile.exists()) {
 				if (replace) {
 					desFile.delete();
 				} else {
-					while (desFile.exists()) {
-						fileName = getNextFile(fileName);
-						desFile = new File(filePath + (filePath.endsWith("/") ? "" : "/") + fileName);
+					if (desFile.exists()) {
+						name = getNextFile(name);
+						desFile = new File(path + (path.endsWith("/") ? "" : "/") + name);
 					}
 				}
 			}
-			createFolder(filePath);
+			createFolder(path);
 			desFile.createNewFile();
-			if (fileContent != null) {
+			if (content != null) {
 				FileWriter resultFile = new FileWriter(desFile);
 				PrintWriter myFile = new PrintWriter(resultFile);
-				String strContent = fileContent;
+				String strContent = content;
 				myFile.println(strContent);
+				myFile.close();
 				resultFile.close();
 			}
+			return desFile;
 		} catch (Exception e) {
-			LOG.error("新建目录操作出错:" + filePath);
+			LOG.error(path + name + ",create file error!");
 			e.printStackTrace();
 			return null;
 		}
-		return desFile;
 	}
 
+	/* ==============delete file=============== */
 	public static boolean deleteFile(String fileName) {
 		return deleteFile(new File(fileName));
 	}
 
 	public static boolean deleteFile(File file) {
+		LOG.debug("=====delete file=====");
 		boolean success = true;
 		if ((file != null) && (file.exists())) {
 			success = file.delete();
-		}
-		if (!success) {
-			LOG.error("无法删除文件：" + file.getAbsolutePath() + File.separator + file.getName());
 		}
 		return success;
 	}
 
 	public static boolean deleteAllFile(String path) {
+		LOG.debug("=====delete all file=====");
 		File file = new File(path);
 		if (!file.exists()) {
 			return false;
@@ -130,45 +134,34 @@ public class FileUtil {
 				deleteFolder(path + "/" + tempList[i]);
 			}
 		}
+		deleteFolder(path);
 		return true;
 	}
 
-	public static String copyFile(String srcFileFullName, String desFolder, String desFileName, boolean replace) {
-		return copyFile(new File(srcFileFullName), desFolder, desFileName, replace);
+	/* ==============copy file=============== */
+	public static String copyFile(String fileFullName, String folder, String name, boolean replace) {
+		return copyFile(new File(fileFullName), folder, name, replace);
 	}
 
-	public static String copyFile(File srcFile, String desFolder, String desFileName, boolean replace) {
-		createFolder(desFolder);
-		String fileName = desFileName;
+	public static String copyFile(File sourceFile, String folder, String name, boolean replace) {
+		createFolder(folder);
+		String fileName = name;
 		if ((fileName == null) || (fileName.equals(""))) {
-			fileName = srcFile.getName();
+			fileName = sourceFile.getName();
 		}
-		StringBuffer desFileFullName = new StringBuffer();
-		desFileFullName.append(desFolder);
-		if (!desFolder.endsWith("/")) {
-			desFileFullName.append("/");
-		}
-		desFileFullName.append(fileName);
-
-		File desFile = new File(desFileFullName.toString());
+		File desFile = new File(folder + (folder.endsWith("/") ? "" : "/") + fileName);
 		if (replace) {
 			desFile.delete();
 		} else {
 			while (desFile.exists()) {
 				fileName = getNextFile(fileName);
-				desFileFullName = new StringBuffer();
-				desFileFullName.append(desFolder);
-				if (!desFolder.endsWith("/")) {
-					desFileFullName.append("/");
-				}
-				desFileFullName.append(fileName);
-				desFile = new File(desFileFullName.toString());
+				desFile = new File(folder + (folder.endsWith("/") ? "" : "/") + fileName);
 			}
 		}
 		InputStream inStream = null;
 		try {
 			int byteread = 0;
-			inStream = new FileInputStream(srcFile);
+			inStream = new FileInputStream(sourceFile);
 			FileOutputStream fs = new FileOutputStream(desFile);
 			byte[] buffer = new byte[1444];
 			while ((byteread = inStream.read(buffer)) != -1) {
@@ -176,7 +169,7 @@ public class FileUtil {
 			}
 			fs.close();
 		} catch (Exception e) {
-			LOG.error("复制单个文件操作出错:" + srcFile.getAbsolutePath() + File.separator + srcFile.getName());
+			LOG.error("copy file error:" + sourceFile.getAbsolutePath() + File.separator + sourceFile.getName());
 			e.printStackTrace();
 			return "";
 		} finally {
@@ -184,14 +177,11 @@ public class FileUtil {
 				try {
 					inStream.close();
 				} catch (IOException e) {
-					LOG.error("复制单个文件Close_InStream操作出错:" + srcFile.getAbsolutePath() + File.separator
-							+ srcFile.getName());
+					LOG.error("file output stream close error:" + sourceFile.getAbsolutePath() + File.separator
+							+ sourceFile.getName());
 					e.printStackTrace();
 				}
 			}
-		}
-		if (LOG.isDebugEnabled()) {
-			LOG.debug("复制文件成功,目标：" + desFileFullName.toString());
 		}
 		return fileName;
 	}
@@ -206,11 +196,7 @@ public class FileUtil {
 			String[] file = a.list();
 			File srcFile = null;
 			for (int i = 0; i < file.length; i++) {
-				if (oldPath.endsWith("/")) {
-					srcFile = new File(oldPath + file[i]);
-				} else {
-					srcFile = new File(oldPath + "/" + file[i]);
-				}
+				srcFile = new File(oldPath + (oldPath.endsWith("/") ? "" : "/") + file[i]);
 				if (srcFile.isFile()) {
 					copyFile(srcFile, newPath, srcFile.getName(), replace);
 				}
@@ -220,81 +206,43 @@ public class FileUtil {
 				}
 			}
 		} catch (Exception e) {
-			LOG.error("复制整个文件夹内容操作出错:" + oldPath + " to " + newPath);
+			LOG.error("copy file folder error:" + oldPath + " to " + newPath);
 			e.printStackTrace();
 			return false;
 		}
 		return true;
 	}
 
-	public static String moveFile(String srcFileFullName, String desFolder, String desFileName, boolean replace)
-			throws Exception {
-		return moveFile(new File(srcFileFullName), desFolder, desFileName, replace);
+	/* ==============move file=============== */
+	public static String moveFile(String fileFullName, String folder, String fileName, boolean replace) {
+		try {
+			return moveFile(new File(fileFullName), folder, fileName, replace);
+		} catch (Exception e) {
+			e.printStackTrace();
+			LOG.error("=====move file error=====");
+			return null;
+		}
 	}
 
-	public static String moveFile(File srcFile, String desFolder, String desFileName, boolean replace)
-			throws Exception {
-		String fileName = copyFile(srcFile, desFolder, desFileName, replace);
+	public static String moveFile(File sourceFile, String folder, String name, boolean replace) throws Exception {
+		String fileName = copyFile(sourceFile, folder, name, replace);
 		if ((fileName != null) && (!fileName.equals(""))) {
-			deleteFile(srcFile);
+			deleteFile(sourceFile);
 		}
 		return fileName;
 	}
 
-	public static String getFileExt(String fileName) {
-		if (fileName == null) {
-			return null;
-		}
-		int pos = fileName.lastIndexOf('.');
-		if (pos > 0) {
-			return fileName.substring(pos + 1, fileName.length());
-		}
-		return "";
-	}
-
-	public static String getFileShortName(String fileName) {
-		if (fileName == null) {
-			return null;
-		}
-		int pos = fileName.lastIndexOf('.');
-		if (pos > 0) {
-			return fileName.substring(0, pos);
-		}
-		return fileName;
-	}
-
-	public static String getNextFile(String fileName) {
-		StringBuffer sb = new StringBuffer();
-
-		String ext = getFileExt(fileName);
-		String shortName = getFileShortName(fileName);
-
-		int pos = shortName.lastIndexOf('[');
-		if ((shortName.endsWith("]")) && (pos > 0)) {
-			String sureName = shortName.substring(0, pos);
-			int index = Integer.valueOf(shortName.substring(pos + 1, shortName.length() - 1)).intValue();
-			sb.append(sureName).append("[").append(index + 1).append("]");
+	public static boolean moveFolder(String oldPath, String newPath) {
+		if (copyFolder(oldPath, newPath, true)) {
+			deleteAllFile(oldPath);
+			return true;
 		} else {
-			sb.append(shortName).append("[1]");
+			LOG.error("=====move file folder error=====");
+			return false;
 		}
-		if ((ext != null) && (!ext.equals(""))) {
-			sb.append(".").append(ext);
-		}
-		return sb.toString();
 	}
 
-	public static String getFileSize(long fileSize) {
-		DecimalFormat df = new DecimalFormat("###,##0.00");
-		double s = 1.0D * fileSize;
-		if (s > 1073741824.0D) {
-			return df.format(s / 1024.0D / 1024.0D / 1024.0D) + "G";
-		}
-		if (s > 1048576.0D) {
-			return df.format(s / 1024.0D / 1024.0D) + "M";
-		}
-		return df.format(s / 1024.0D) + "K";
-	}
-
+	/* ==============lock file=============== */
 	public static void simpleLockFile(String fileName) {
 		try {
 			File file = new File(fileName);
@@ -355,67 +303,29 @@ public class FileUtil {
 		return result.toByteArray();
 	}
 
-	public static List<String> findAllFiles(String path) {
-		List<String> result = new ArrayList<String>();
-		File file = new File(path);
-		return listFile(file, result);
-	}
-
-	private static List<String> listFile(File file, List<String> result) {
-		if (file.isDirectory()) {
-			File[] files = file.listFiles();
-			for (int i = 0; i < files.length; i++) {
-				listFile(files[i], result);
-			}
-		} else {
-			String filePath = file.getAbsolutePath();
-			result.add(filePath);
-		}
-		return result;
-	}
-
-	public static void moveFile(String sFilePathName, String toPath) throws Exception {
-		File sFile = new File(sFilePathName);
-		if (!sFile.exists()) {
-			throw new Exception("源文件[" + sFile.getPath() + "]不存在。");
-		}
-		File tFile = new File(toPath);
-		if (!tFile.exists()) {
-			tFile.mkdirs();
-		}
-		tFile = new File(toPath + "/" + sFile.getName());
-		FileOutputStream fos = null;
-		FileInputStream fis = null;
+	/* ==============zip/unzip file=============== */
+	public static Zip zip(String folder, String zipName) {
 		try {
-			fos = new FileOutputStream(tFile);
-			fis = new FileInputStream(sFile);
-			byte[] buffer = new byte[BUFFER_SIZE];
-			int len = 0;
-			while ((len = fis.read(buffer)) > 0) {
-				fos.write(buffer, 0, len);
+			ZipOutputStream out = new ZipOutputStream(
+					new BufferedOutputStream(new FileOutputStream(zipName), BUFFER_SIZE));
+			Zipper zipper = new Zipper();
+			Zip zro = new Zip();
+			File zipFile = new File(folder);
+			try {
+				if (!zipFile.exists()) {
+					zipFile.createNewFile();
+				}
+				zipper.zip(out, zipFile, "");
+			} finally {
+				out.close();
 			}
-		} finally {
-			fos.close();
-			fis.close();
+			zro.setContentSize(Long.valueOf(zipper.getContentSize()));
+			zro.setZipFileSize(Long.valueOf(zipFile.length()));
+			return zro;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
 		}
-		sFile.delete();
-	}
-
-	public static Zip zip(String zipFilePathName, String inputFileName) throws Exception {
-		ZipOutputStream out = new ZipOutputStream(
-				new BufferedOutputStream(new FileOutputStream(zipFilePathName), BUFFER_SIZE));
-		Zipper zipper = new Zipper();
-		Zip zro = new Zip();
-		File zipFile = new File(inputFileName);
-		try {
-			zipper.zip(out, zipFile, "");
-		} finally {
-			out.close();
-		}
-		zro.setContentSize(Long.valueOf(zipper.getContentSize()));
-		zipFile = new File(zipFilePathName);
-		zro.setZipFileSize(Long.valueOf(zipFile.length()));
-		return zro;
 	}
 
 	private static class Zipper {
@@ -502,5 +412,80 @@ public class FileUtil {
 				zipFile.close();
 			}
 		}
+	}
+
+	/* =============================== */
+	/* ============utilities============= */
+	/* =============================== */
+	public static String getFileExt(String fileName) {
+		if (fileName == null) {
+			return null;
+		}
+		int pos = fileName.lastIndexOf('.');
+		if (pos > 0) {
+			return fileName.substring(pos + 1, fileName.length());
+		}
+		return "";
+	}
+
+	public static String getFileShortName(String fileName) {
+		if (fileName == null) {
+			return null;
+		}
+		int pos = fileName.lastIndexOf('.');
+		if (pos > 0) {
+			return fileName.substring(0, pos);
+		}
+		return fileName;
+	}
+
+	public static String getNextFile(String fileName) {
+		StringBuffer sb = new StringBuffer();
+		String ext = getFileExt(fileName);
+		String shortName = getFileShortName(fileName);
+
+		int pos = shortName.lastIndexOf('[');
+		if ((shortName.endsWith("]")) && (pos > 0)) {
+			String sureName = shortName.substring(0, pos);
+			int index = Integer.valueOf(shortName.substring(pos + 1, shortName.length() - 1)).intValue();
+			sb.append(sureName).append("[").append(index + 1).append("]");
+		} else {
+			sb.append(shortName).append("[1]");
+		}
+		if ((ext != null) && (!ext.equals(""))) {
+			sb.append(".").append(ext);
+		}
+		return sb.toString();
+	}
+
+	public static String getFileSize(long fileSize) {
+		DecimalFormat df = new DecimalFormat("###,##0.00");
+		double s = 1.0D * fileSize;
+		if (s > 1073741824.0D) {
+			return df.format(s / 1024.0D / 1024.0D / 1024.0D) + "G";
+		}
+		if (s > 1048576.0D) {
+			return df.format(s / 1024.0D / 1024.0D) + "M";
+		}
+		return df.format(s / 1024.0D) + "K";
+	}
+
+	public static List<String> findAllFiles(String path) {
+		List<String> result = new ArrayList<String>();
+		File file = new File(path);
+		return listFile(file, result);
+	}
+
+	private static List<String> listFile(File file, List<String> result) {
+		if (file.isDirectory()) {
+			File[] files = file.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				listFile(files[i], result);
+			}
+		} else {
+			String filePath = file.getAbsolutePath();
+			result.add(filePath);
+		}
+		return result;
 	}
 }
