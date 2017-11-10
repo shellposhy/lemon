0f1e17c3f80edb1fb7d415c45c6a7bbb29fb0623
shishb <shishb@pku.edu.cn>
2017-11-10 16:57:29 +0800

    BarcodeUtil

--- src/main/java/cn/com/lemon/util/barcode/BarcodeUtil.java
+++ src/main/java/cn/com/lemon/util/barcode/BarcodeUtil.java
@@ -1,5 +1,7 @@
 package cn.com.lemon.util.barcode;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,12 +10,23 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.imageio.ImageIO;
+
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Binarizer;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
 import com.google.zxing.EncodeHintType;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
 import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
 
 import sun.misc.BASE64Encoder;
 
@@ -99,6 +112,33 @@ public final class BarcodeUtil {
 			e.printStackTrace();
 			return null;
 		} catch (WriterException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * Decode the image content and return
+	 * 
+	 * @param picPath
+	 *            the decode image path
+	 * @return {@code String} the real content of the code image
+	 */
+	public static String Decode(String picPath) {
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(new File(picPath));
+			LuminanceSource source = new BufferedImageLuminanceSource(image);
+			Binarizer binarizer = new HybridBinarizer(source);
+			BinaryBitmap binaryBitmap = new BinaryBitmap(binarizer);
+			Map<DecodeHintType, Object> hints = new HashMap<DecodeHintType, Object>();
+			hints.put(DecodeHintType.CHARACTER_SET, "UTF-8");
+			Result result = new MultiFormatReader().decode(binaryBitmap, hints);
+			return result.getText();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} catch (NotFoundException e) {
 			e.printStackTrace();
 			return null;
 		}
