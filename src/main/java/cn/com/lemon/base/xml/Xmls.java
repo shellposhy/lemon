4707a72a62626fd3ed60295bc3e6245f056f123a
shishb <shishb@pku.edu.cn>
2018-02-01 13:44:31 +0800

    add xml utilities

---
+++ src/main/java/cn/com/lemon/base/xml/Xmls.java
@@ -0,0 +1,53 @@
+package cn.com.lemon.base.xml;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import static cn.com.lemon.base.Preasserts.checkArgument;
+import static cn.com.lemon.base.Strings.isNullOrEmpty;
+
+import com.thoughtworks.xstream.XStream;
+
+public final class Xmls {
+	private static final Map<String, XStream> XSTREAMS = new ConcurrentHashMap<String, XStream>();
+	private static final String DEFAULT_NAME = "FALSE";
+	private static final String DEFAULT_XML_HEADER = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
+
+	private Xmls() {
+	}
+
+	static {
+		XSTREAMS.put("TRUE", new XStream(new XmlsDriver(true)));
+		XSTREAMS.put("FALSE", new XStream(new XmlsDriver(false)));
+	}
+
+	public static XStream newInstance() {
+		return newInstance(DEFAULT_NAME);
+	}
+
+	public static XStream newInstance(String name) {
+		XStream stream = XSTREAMS.get(name);
+		checkArgument(stream != null);
+		return stream;
+	}
+
+	public static String generator(Object data, boolean isUseCDATA, Class<?> clazz) {
+		if (null == data)
+			return null;
+		XStream xstream = isUseCDATA ? newInstance("TRUE") : newInstance();
+		xstream.processAnnotations(clazz);
+		xstream.autodetectAnnotations(true);
+		return DEFAULT_XML_HEADER + xstream.toXML(data);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T xml2Obj(String data, boolean isUseCDATA, Class<?> clazz) {
+		if (isNullOrEmpty(data)) {
+			return null;
+		}
+		XStream xstream = isUseCDATA ? newInstance("TRUE") : newInstance();
+		if (clazz != null) {
+			xstream.processAnnotations(clazz);
+		}
+		return (T) xstream.fromXML(data);
+	}
+}
---
+++ src/main/java/cn/com/lemon/base/xml/XmlsDriver.java
@@ -0,0 +1,45 @@
+package cn.com.lemon.base.xml;
+
+import java.io.Writer;
+
+import com.thoughtworks.xstream.core.util.QuickWriter;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
+import com.thoughtworks.xstream.io.xml.XmlFriendlyNameCoder;
+import com.thoughtworks.xstream.io.xml.XppDriver;
+
+public class XmlsDriver extends XppDriver {
+
+	private boolean isUseCDATA = false;
+
+	public XmlsDriver() {
+		super(new XmlFriendlyNameCoder("__", "_"));
+	}
+
+	public XmlsDriver(boolean isUseCDATA) {
+		super(new XmlFriendlyNameCoder("__", "_"));
+		this.isUseCDATA = isUseCDATA;
+	}
+
+	@Override
+	public HierarchicalStreamWriter createWriter(Writer out) {
+		if (!isUseCDATA) {
+			return super.createWriter(out);
+		}
+		return new PrettyPrintWriter(out) {
+			@Override
+			public void startNode(String name, @SuppressWarnings("rawtypes") Class clazz) {
+				super.startNode(name, clazz);
+			}
+
+			@Override
+			protected void writeText(QuickWriter writer, String text) {
+				if (isUseCDATA) {
+					writer.write("<![CDATA[" + text + "]]>");
+				} else {
+					writer.write(text);
+				}
+			}
+		};
+	}
+}
