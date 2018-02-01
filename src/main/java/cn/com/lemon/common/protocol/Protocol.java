fb747a20c1b22a056b7d15f8d3b7c87adc51379a
shishb <shishb@pku.edu.cn>
2018-02-01 14:41:07 +0800

    add Protocol model

---
+++ src/main/java/cn/com/lemon/common/protocol/Protocol.java
@@ -0,0 +1,20 @@
+package cn.com.lemon.common.protocol;
+
+/**
+ * The base bean {@code Protocol}
+ * 
+ * @author shellpo shih
+ * @version 1.0
+ */
+public class Protocol {
+	private String version;
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+}
