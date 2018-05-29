8762e73a6c13bddd03e04869fe57995797d79016
shishaobo <shishaobo@zgshfp.com.cn>
2018-05-29 19:56:52 +0800

    update

--- src/test/java/cn/com/lemon/test/ImportPublishFinish.java
+++ src/test/java/cn/com/lemon/test/ImportPublishFinish.java
@@ -6,13 +6,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import cn.com.lemon.base.DateUtil;
 import cn.com.lemon.base.poi.Excels;
-import cn.com.lemon.base.util.Jsons;
 import cn.com.lemon.common.connection.Oracles;
 
 public class ImportPublishFinish {
@@ -25,62 +21,23 @@ public class ImportPublishFinish {
 	public static void importData() throws SQLException, IOException {
 		Connection connection = Oracles.newInstance();
 		List<String[]> datas = Excels.read(new File("C:\\Users\\Administrator\\Desktop\\11.xlsx"), true);
-		String sql = "INSERT INTO MY_PUBLISH_STATUS_BACK (id,status,fail,txdate) VALUES(?,?,?,?)";
+		String sql = "INSERT INTO my_publish_setnum (ID,DATESTR,SETNUM) VALUES(?,?,?)";
 		PreparedStatement ps = connection.prepareStatement(sql);
-		Map<Integer, Account> result = new HashMap<Integer, Account>();
-		int z = 0;
-		int s = 0;
-		int f = 0;
-		int o = 0;
+		String dateStr = datas.get(0)[1];
+		int tmp = 1;
 		for (String[] data : datas) {
-			System.out.println(Jsons.json(data));
-			if (!result.containsKey(Integer.valueOf(data[0]))) {
-				Account account = new Account();
-				account.setId(Integer.valueOf(data[0]));
-				if (data[1].trim().equals("打款中")) {
-					account.setStatus(3);
-					z++;
-				} else if (data[1].trim().equals("成功")) {
-					account.setStatus(4);
-					s++;
-				} else if (data[1].trim().equals("失败")) {
-					account.setStatus(5);
-					f++;
-				} else {
-					account.setStatus(0);
-					o++;
-				}
-				if (data.length == 3) {
-					if (data[2].trim().equals("账号原因")) {
-						account.setFail(5);
-					} else {
-						account.setFail(9);
-					}
-				} else {
-					account.setFail(0);
-				}
-				if (data.length == 4) {
-					Date date=DateUtil.parse(data[3], "yyyy-MM-dd");
-					System.out.println(data[3]);
-					account.setTxdate(date);
-				}
-				result.put(Integer.valueOf(data[0]), account);
+			ps.setInt(1, Integer.valueOf(data[0]));
+			ps.setString(2, data[1]);
+			if (dateStr.equals(data[1])) {
+				ps.setString(3, data[1] + "-" + tmp);
+				tmp++;
+			} else {
+				tmp = 1;
+				dateStr = data[1];
+				ps.setString(3, data[1] + "-" + tmp);
+				tmp++;
 			}
-		}
-		System.out.println("total=" + datas.size());
-		System.out.println("size=" + result.size());
-		int t = z + s + f + o;
-		System.out.println("付款中=" + z);
-		System.out.println("成功=" + s);
-		System.out.println("失败=" + f);
-		System.out.println("其他=" + o);
-		System.out.println("数据统计=" + t);
-		for (Map.Entry<Integer, Account> entry : result.entrySet()) {
-			ps.setInt(1, entry.getKey());
-			ps.setInt(2, entry.getValue().getStatus());
-			ps.setInt(3, entry.getValue().getFail());
-			ps.setDate(4,new java.sql.Date(entry.getValue().getTxdate().getTime()) );
-			 ps.executeQuery();
+			ps.executeQuery();
 		}
 	}
 }
--- src/test/java/cn/com/lemon/test/Test.java
+++
@@ -1,9 +0,0 @@
-package cn.com.lemon.test;
-
-import java.util.Date;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println(new Date(Long.parseLong("1534216635817")));
-	}
-}
