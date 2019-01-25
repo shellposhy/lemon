511b55d4b91aa272b5e34dac6ebc372296857795
shishaobo <shishaobo@zgshfp.com.cn>
2019-01-25 17:53:29 +0800

    add test

--- src/test/java/cn/com/lemon/test/Test.java
+++ src/test/java/cn/com/lemon/test/Test.java
@@ -4,5 +4,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("儿子患肿瘤：妻子放弃，我一定要坚持下去！".length());
+
+		System.out.println("522632017000".substring(6, 9));
+
+		String test = "/content";
+		System.out.println(test.startsWith("/") ? test.substring(1) : test);
 	}
 }
