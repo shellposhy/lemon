3d9048324340c3de2ce886bf9d4d7e65deb6f510
shishaobo <shishaobo@zgshfp.com.cn>
2019-05-27 16:18:59 +0800

    update xxe not public instance

--- src/main/java/cn/com/lemon/base/util/Xxes.java
+++ src/main/java/cn/com/lemon/base/util/Xxes.java
@@ -3,6 +3,14 @@ package cn.com.lemon.base.util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static cn.com.lemon.base.Strings.isNullOrEmpty;
+
+/**
+ * Simple filter XXE third URL and sensitive words
+ * 
+ * @author shaobo shih
+ * @version 1.0
+ */
 public final class Xxes {
 	private final static Pattern URL_PATTERN = Pattern
 			.compile("\\b(([\\w-]+://?|www[.])[^\\s()<>]+(?:[\\w\\d]+|([^[:punct:]\\s]|/)))");
@@ -11,6 +19,9 @@ public final class Xxes {
 	}
 
 	public static String xxe(String content) {
+		if (isNullOrEmpty(content))
+			return null;
+		// filter third URL
 		Matcher matcher = URL_PATTERN.matcher(content);
 		if (matcher.find()) {
 			if (matcher.groupCount() == 1) {
@@ -21,7 +32,8 @@ public final class Xxes {
 				}
 			}
 		}
-		if (content != null && content.length() > 0) {
+		// filter sensitive word
+		if (!isNullOrEmpty(content)) {
 			content = content.replace("DOCTYPE", "").replace("SYSTEM", "").replace("ENTITY", "").replace("PUBLIC", "");
 		}
 		return content;
